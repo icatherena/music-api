cbfa0be0f2cfcded33c015b4ea909feb7b4cceeb
icatherena <beresovskyc@gmail.com>
2023-06-12 09:05:18 -0300

    implementing API routes, get and post, for artist and post

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,93 @@
 import { PrismaClient } from '@prisma/client'
+import express from 'express'
 
 const prisma = new PrismaClient()
+const app = express()
+const port = 3000;
 
-async function main() {
-  const newArtist = await prisma.artist.create({
-    data: {
-      name: 'Santana',
+app.use(express.json())
+
+// Retrieve all available artists
+app.get(`/artist`, async (req, res) => {
+  const artists = await prisma.artist.findMany()
+  res.json(artists)
+})
+
+// Retrieve a specific artist by its ID. Includes all its released music sorted by date
+app.get(`/artist/:artist_id`, async (req, res) => {
+  const artist_id = Number(req.params.artist_id)
+  const artist = await prisma.artist.findUnique({
+    where: { 
+      id: artist_id 
+    },
+    include: { 
       post: {
-        create: {
-          type: 'SINGLE',
-          name: 'Smooth',
-          post_date: new Date('1999-06-15T00:00:00Z'), 
-          // newDate constructor is used to initialize a date object with the proper DateTime format
-          // Must be used in order to provide a DateTime value instead of string one
-          // 'T' is a separator indicating that the time follows 
-          // 'Z' at the end denotes the time zone. In this case, it represents UTC (Coordinated Universal Time)
-          // The letter 'Z' stands for "Zulu time," which is another term for UTC 
-          // DO I HAVE TO CHANGE THE TIME ZONE?
-        },
-      },
+        orderBy: {
+          post_date: 'desc',
+        }
+      }
     },
   })
-  console.log('Created new artist: ', newArtist)
+  res.json(artist)
+})
 
-  const allArtists = await prisma.artist.findMany({
-    include: { post: true },
+// Create a new artist
+/* app.post('/artist', async (req, res) => {
+  const { name } = req.body.name
+  const result = await prisma.artist.create({
+    data: { 
+      name,
+    },
   })
-  console.log('All artists: ')
-  console.dir(allArtists, { depth: null })
-}
+  res.json(result)
+}) */
 
-main()
-  .catch((e) => console.error(e))
-  .finally(async () => await prisma.$disconnect())
+app.post(`/artist`, async (req, res) => {
+  const result = await prisma.artist.create({
+    data: { ...req.body },
+  })
+  res.json(result)
+})
+
+// Retrieve all released music from any artist
+app.get(`/post/`, async (req, res) => {
+  const posts = await prisma.post.findMany({
+    include: {
+      theme: {}
+    }
+  })
+  res.json(posts)
+})
+
+// Retrieve any released music by its ID. Includes number of songs, total length of the album in seconds plus all songs
+app.get(`/post/:post_id`, async (req, res) => {
+  const post_id = Number(req.params.post_id)
+  const post = await prisma.post.findUnique({
+    where: { 
+      id: post_id 
+    },
+    include: { 
+      theme: {}
+    },
+  })
+  res.json(post)
+})
+
+// Create a new release, including all its theme' songs
+app.post(`/post`, async (req, res) => {
+  const result = await prisma.post.create({
+    data: { ...req.body },
+  })
+  res.json(result)
+})
+
+app.post(`/theme`, async (req, res) => {
+  const result = await prisma.theme.create({ 
+    data: { ...req.body },
+  })
+  res.json(result)
+})
+
+app.listen(port, () =>
+  console.log(`REST API server ready on port ${port}`),
+)
